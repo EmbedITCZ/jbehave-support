61adadc73eb7565c05d15bb761cf39e4d136c348
Pavel Jandejsek <pavel.jandejsek@gmail.com>
2019-07-31 17:05:23 +0200

    Added support for directories in report uploading script.

--- ci/report_upload.sh
+++ ci/report_upload.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    zip -r -j reports.zip jbehave-support-core/target/reports/*
+    (cd jbehave-support-core/target/reports && zip -r ../../../reports.zip .)
     curl -F "file=@reports.zip;type=application/zip" -X POST -H "Content-Type: multipart/form-data" -u ${JBUS_REPORT_SERVICE_AUTHENTICATION} ${JBUS_REPORT_SERVICE_ADDRESS}/api/reports/${TRAVIS_PULL_REQUEST}
 fi

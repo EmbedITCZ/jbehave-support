f78fb033472272639b45aa44e4f2c0780b86236a
Pavel Jandejsek <pavel.jandejsek@gmail.com>
2020-02-10 18:33:53 +0100

    Removed Travis pipeline.

--- ci/browserstack.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mvn verify -DskipUnitTests=true -Djbehave.report.level=STORY -Dbrowser-stack.username=${BROWSERSTACK_USER} -Dbrowser-stack.key=${BROWSERSTACK_KEY} -Dit.test=**/BrowserStackIT -Pintegration-test -B -V
--- ci/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mvn org.jacoco:jacoco-maven-plugin:prepare-agent test org.jacoco:jacoco-maven-plugin:report -B -V && \
-curl --silent -o jbehave-support-core/target/codacy-reporter-latest.jar -L https://github.com/codacy/codacy-coverage-reporter/releases/download/4.0.3/codacy-coverage-reporter-4.0.3-assembly.jar && \
-java -jar jbehave-support-core/target/codacy-reporter-latest.jar report -l Java -r jbehave-support-core/target/site/jacoco/jacoco.xml --partial

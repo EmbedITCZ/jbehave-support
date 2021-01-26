adf27cf75b1de28bfcf67a1c880be1eb48b392c6
Pavel Jandejsek <pavel.jandejsek@gmail.com>
2021-01-26 21:17:28 +0100

    Added verifiers for "lower or equal" + "great or equal".

---
+++ jbehave-support-core/src/main/java/org/jbehavesupport/core/internal/verification/LowerThanOrEqualVerifier.java
@@ -0,0 +1,33 @@
+package org.jbehavesupport.core.internal.verification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Component;
+
+import static org.jbehavesupport.core.verification.VerifierNames.LE;
+import static org.springframework.util.Assert.isInstanceOf;
+import static org.springframework.util.Assert.isTrue;
+
+@Component
+@RequiredArgsConstructor
+public final class LowerThanOrEqualVerifier extends AbstractVerifier {
+
+    final ConversionService conversionService;
+
+    @Override
+    public String name() {
+        return LE;
+    }
+
+    @Override
+    public void verify(Object actual, Object expected) {
+        isInstanceOf(Comparable.class, actual, "compared value must be of type Comparable");
+        isTrue(conversionService.canConvert(expected.getClass(), actual.getClass()), expected + " can not be converted to " + actual.getClass());
+
+        Object expectedComparable = conversionService.convert(expected, actual.getClass());
+        if (((Comparable) actual).compareTo(expectedComparable) > 0) {
+            throwAssertionError("value '%s' is not lower than or equal to '%s'", actual, expected);
+        }
+    }
+
+}

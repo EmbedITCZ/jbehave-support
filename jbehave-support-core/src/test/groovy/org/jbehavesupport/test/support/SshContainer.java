61bf156130a9585aa0f4cec217b3daf740b4e431
Pavel Jandejsek <pavel.jandejsek@gmail.com>
2021-07-07 20:55:31 +0200

    Switched sshd image to testcontainers/sshd.

--- jbehave-support-core/src/test/groovy/org/jbehavesupport/test/support/SshContainer.java
+++ jbehave-support-core/src/test/groovy/org/jbehavesupport/test/support/SshContainer.java
@@ -3,12 +3,10 @@ package org.jbehavesupport.test.support;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 
 public class SshContainer extends GenericContainer<SshContainer> {
 
-    private static final String IMAGE_VERSION = "panubo/sshd:1.4.0";
-    private static SshContainer container;
+    private static final String IMAGE_VERSION = "testcontainers/sshd:1.0.0";
 
     public SshContainer() {
         super(IMAGE_VERSION);
@@ -16,12 +14,14 @@ public class SshContainer extends GenericContainer<SshContainer> {
 
     @Override
     public void start() {
-        withEnv("SSH_USERS", "sa:1000:1000")
-            .withEnv("SSH_ENABLE_PASSWORD_AUTH", "true")
-            .withClasspathResourceMapping("ssh/user-setup.sh", "/etc/entrypoint.d/", BindMode.READ_ONLY)
-            .withClasspathResourceMapping("ssh/key.pub", "/etc/authorized_keys/sa", BindMode.READ_ONLY)
-            .waitingFor(Wait.forLogMessage(".*Server listening on :: port 22.*\\n", 1))
-            .withExposedPorts(22);
+        withExposedPorts(22)
+            .withClasspathResourceMapping("ssh/key.pub", "/etc/authorized_keys/root", BindMode.READ_ONLY)
+            .withCommand(
+                "sh",
+                "-c",
+                // Disable ipv6 & Make it listen on all interfaces, not just localhost
+                "echo \"root:sa\" | chpasswd && /usr/sbin/sshd -D -o PermitRootLogin=yes -o AddressFamily=inet -o GatewayPorts=yes"
+            );
         super.start();
     }
 
@@ -33,7 +33,7 @@ public class SshContainer extends GenericContainer<SshContainer> {
     public void updateDynamicPropertyRegistry(DynamicPropertyRegistry registry) {
         registry.add("ssh.hostname", this::getHost);
         registry.add("ssh.port", () -> getMappedPort(22));
-        registry.add("ssh.credentials.user", () -> "sa");
+        registry.add("ssh.credentials.user", () -> "root");
         registry.add("ssh.credentials.password", () -> "sa");
         registry.add("ssh.credentials.keyPath", () -> "classpath:/ssh/key");
     }

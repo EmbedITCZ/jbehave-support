fbaaeec3f213a19a92c6e2d1f2e8533cd98c6776
romcooo.desktop2 <stubna@gmail.com>
2021-11-11 13:50:44 +0100

    - changed conversion method - Double.valueOf(it.toString()) to conversionService.convert(it, Double.class)

--- jbehave-support-core/src/main/java/org/jbehavesupport/core/internal/verification/MathEqualsVerifier.java
+++ jbehave-support-core/src/main/java/org/jbehavesupport/core/internal/verification/MathEqualsVerifier.java
@@ -1,14 +1,21 @@
 package org.jbehavesupport.core.internal.verification;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.ConversionException;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 import static org.jbehavesupport.core.verification.VerifierNames.MATH_EQ;
 
 @Component
+@RequiredArgsConstructor
 public final class MathEqualsVerifier extends AbstractVerifier {
 
     public static final String MESSAGE_PARSE_OK = "value %s (originally written as '%s') is not mathematically equal to %s (originally written as '%s')";
     public static final String MESSAGE_PARSE_NOK = "couldn't parse either or both value/-s to a number: '%s', '%s'";
+    final ConversionService conversionService;
 
     @Override
     public String name() {
@@ -22,13 +29,13 @@ public final class MathEqualsVerifier extends AbstractVerifier {
         }
 
         try {
-            Double actualDouble = Double.valueOf(actual.toString());
-            Double expectedDouble = Double.valueOf(expected.toString());
-            if (actualDouble.equals(expectedDouble)) {
+            Double actualDouble = conversionService.convert(actual, Double.class);
+            Double expectedDouble = conversionService.convert(expected, Double.class);
+            if (Objects.equals(actualDouble, expectedDouble)) {
                 return;
             }
             throwAssertionError(MESSAGE_PARSE_OK, actualDouble, actual, expectedDouble, expected);
-        } catch (NumberFormatException e) {
+        } catch (ConversionException e) {
             throwAssertionError(MESSAGE_PARSE_NOK, actual, expected);
         }
     }

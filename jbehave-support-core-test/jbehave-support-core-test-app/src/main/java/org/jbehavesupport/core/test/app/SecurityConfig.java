8871eaa09d6b9c0de45151e3075b80731e67d136
Pavel Jandejsek <pavel.jandejsek@gmail.com>
2023-09-30 20:05:31 +0200

    Upgrade spring boot to latest 3.1.4
    Webdriver manager also updated due to conflicts in http client 5 versions. Selenium as well due to issues with left clicking.

--- jbehave-support-core-test/jbehave-support-core-test-app/src/main/java/org/jbehavesupport/core/test/app/SecurityConfig.java
+++ jbehave-support-core-test/jbehave-support-core-test-app/src/main/java/org/jbehavesupport/core/test/app/SecurityConfig.java
@@ -2,22 +2,28 @@ package org.jbehavesupport.core.test.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain basicAuthFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-            .authorizeHttpRequests()
-            .requestMatchers("/rest/secure/**").fullyAuthenticated()
-            .requestMatchers("/**").permitAll()
-            .and()
-            .httpBasic()
-            .and().headers().frameOptions().sameOrigin()
-            .and().build();
+        return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(matcherRegistry ->
+                matcherRegistry
+                    .requestMatchers(antMatcher("/rest/secure/**")).fullyAuthenticated()
+                    .requestMatchers(antMatcher("/**")).permitAll()
+            )
+            .httpBasic(Customizer.withDefaults())
+            .headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+            .build();
     }
 
 }
